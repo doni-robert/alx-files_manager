1af3e822496b2dd672de23fffe706ff995095223
doni-robert <irobertndungu@gmail.com>
2023-12-06 17:45:36 +0300

    task 0 and 1

--- utils/redis.js
+++ utils/redis.js
@@ -3,39 +3,40 @@
  */
 
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor(){
-        this.client = createClient();
-        this.client.on('error', (err) => console.log('Error:', err));
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
 
-    }
+    this.client.on('error', (error) => {
+      console.error('Redis client not connected to the server:', error.message);
+      this.isClientConnected = false;
+    });
 
-    isAlive(){
-        this.client.on('connect', (err) => {
-            if (err){
-                return false;
-            }
-            return true;
-        })
-    }
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
 
-    async get(key){
-        return await this.client.get(key);
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-    }
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
 
-    async set(key, value, time){
-        await this.client.setex(key, time, value);
+  async set(key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
 
-    }
-
-    async  del(key){
-        await this.client.del(key);
-
-    }
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
 }
 
 const redisClient = new RedisClient();
 
-module.exports = redisClient;
+export default redisClient;
